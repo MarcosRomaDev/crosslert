a1e4d174e373d22f8597f7d4752dd9377edeb53e
MarcosRomaDev <marcos.roma.dev@gmail.com>
2026-07-30 23:13:30 +0200

    chore: scaffolding inicial del backend con Spring Boot

---
+++ crosslert-backend/src/main/java/com/example/crosslert_backend/CrosslertBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.crosslert_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrosslertBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrosslertBackendApplication.class, args);
+	}
+
+}
---
+++ crosslert-backend/src/test/java/com/example/crosslert_backend/CrosslertBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crosslert_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrosslertBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
