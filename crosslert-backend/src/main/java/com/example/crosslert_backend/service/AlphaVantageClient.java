11683c6462c9ff3d108bf92c8f4ed7af520f9df2
MarcosRomaDev <marcos.roma.dev@gmail.com>
2026-08-02 14:46:34 +0200

    feat: cliente AlphaVantageClient para el endpoint TIME_SERIES_DAILY

---
+++ crosslert-backend/src/main/java/com/example/crosslert_backend/service/AlphaVantageClient.java
@@ -0,0 +1,31 @@
+package com.example.crosslert_backend.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class AlphaVantageClient {
+    private final RestClient restClient;
+    @Value("${alpha.vantage.api.key}")
+    private String apiKey;
+
+    public AlphaVantageClient(RestClient.Builder builder) {
+        this.restClient = builder.baseUrl("https://www.alphavantage.co").build();
+    }   
+
+    
+    public String getStockData(String symbol) {
+        String data = restClient.get()
+                        .uri(uriBuilder -> uriBuilder
+                            .path("/query")
+                            .queryParam("function", "TIME_SERIES_DAILY")
+                            .queryParam("symbol", symbol).queryParam("apikey", apiKey)
+                            .build())
+                        .retrieve()
+                        .body(String.class);
+        return data;
+    }
+
+    
+}
