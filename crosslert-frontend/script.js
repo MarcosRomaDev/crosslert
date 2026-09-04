931fbb0ee87a8ef32cf424f643545aa59480281a
MarcosRomaDev <marcos.roma.dev@gmail.com>
2026-09-04 17:54:42 +0200

    feat: ficha del activo con gráfica de precio historica

--- crosslert-frontend/script.js
+++ crosslert-frontend/script.js
@@ -1,6 +1,9 @@
 const tickerForm = document.getElementById("ticker-form");
 const tickerInput = document.getElementById("ticker");
 const tickerSearchResult = document.getElementById("result");
+const chartCanvas = document.getElementById("chart");
+
+// Busqueda de symbolos
 
 tickerForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -14,8 +17,34 @@ tickerForm.addEventListener("submit", (event) => {
         let symbol = item.symbol;
         let name = item.name;
         let node = document.createElement("li");
+        node.dataset.symbol = item.symbol;
         node.textContent = symbol + " - " + name;
+        node.addEventListener("click", () => {
+          let selected = node.dataset.symbol;
+          console.log(selected);
+          loadCandles(node.dataset.symbol);
+        });
         tickerSearchResult.appendChild(node);
       });
     });
 });
+
+// Candle Fetch
+
+function loadCandles(symbol) {
+  fetch("http://localhost:8080/api/candles/" + symbol)
+    .then((Response) => Response.json())
+    .then((data) => {
+      console.log(data);
+      data.sort((a, b) => a.id.date.localeCompare(b.id.date));
+      let date = data.map((candle) => candle.id.date);
+      let close = data.map((candle) => candle.close);
+      new Chart(chartCanvas, {
+        type: "line",
+        data: {
+          labels: date,
+          datasets: [{ label: "Close", data: close }],
+        },
+      });
+    });
+}
